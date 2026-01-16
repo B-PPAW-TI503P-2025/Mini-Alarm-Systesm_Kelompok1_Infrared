2aa924e6e841b3ee1835fc6bc9af356bb81d52a0
nisrina salsa <nisrinahikmawan@gmail.com>
2026-01-16 17:17:00 +0700

    index model untuk export

---
+++ backend/models/index.js
@@ -0,0 +1,9 @@
+const User = require('./User'); // Tambahkan ini
+const Sensor = require('./Sensor');
+const SensorLog = require('./SensorLog');
+
+module.exports = {
+  User,
+  Sensor,
+  SensorLog
+};
